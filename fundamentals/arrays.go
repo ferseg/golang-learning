7560bdc4a478bdd66ae3c328becff922ffefb05b
Fernando Segovia <fernandosegovia25@gmail.com>
2022-10-27 17:24:36 -0600

    Array and slice example

---
+++ fundamentals/arrays.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+)
+
+func main()  {
+  var arr = [...]int{1, 2, 3}
+  fmt.Println(arr)
+
+
+  arrCopy := arr
+  arrCopy[1] = 5
+
+  fmt.Println(arr)
+  fmt.Println(arrCopy)
+
+  arrRef := &arr
+  arrRef[2] = 4
+
+  fmt.Println(arr)
+  fmt.Println(arrRef)
+
+  thisIsASlice := []int{1,2,3}
+  sliceRef := thisIsASlice
+  sliceRef[0] = 10
+
+  fmt.Println(thisIsASlice)
+  fmt.Println(sliceRef)
+
+}
